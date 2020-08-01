49851aa1bc6a99bd0677d7cc2a934e6ff3964f60
Jessy Halife <jesica.halife@kern-it.com.ar>
2020-08-01 15:02:20 -0300

    mail fix

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -3,7 +3,7 @@ import { sendMail } from "../../lib/emailSender";
 
 export default (req, res) => {
   const { email, title, message } = req.body;
-  console.log(req.body);
+
   if (sendMail({ email, title, message })) res.send(200);
   else res.send(400);
 };
