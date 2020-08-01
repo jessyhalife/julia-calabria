bd49f845922c798fb97d9f95089ab0feeb1eb7f1
Jessy Halife <jesica.halife@kern-it.com.ar>
2020-08-01 15:16:32 -0300

    mail fix

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,9 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { sendMail } from "../../lib/emailSender";
 
-export default (req, res) => {
+export default async (req, res) => {
   const { email, title, message } = req.body;
+  const rta = await sendMail({ email, title, message });
+  if (rta) return res.status(200).send("");
 
-  if (sendMail({ email, title, message })) res.send(200);
-  else res.send(400);
+  return res.status(400).send("error");
 };
