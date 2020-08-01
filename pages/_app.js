441d4717829be57df993a6aa3c5aa7f052125f7b
Jessy Halife <jesica.halife@kern-it.com.ar>
2020-08-01 13:34:38 -0300

    svg icons

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,8 @@ import "@fortawesome/fontawesome-free/js/fontawesome";
 import "@fortawesome/fontawesome-free/js/solid";
 import "@fortawesome/fontawesome-free/js/regular";
 import "@fortawesome/fontawesome-free/js/brands";
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false; /* eslint-disable import/first */
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
